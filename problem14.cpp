7ffabf3167e9ab98df232b0023b47beab83a4f21
a5897101 <99851679+a5897101@users.noreply.github.com>
2023-03-21 23:08:07 +0800

    Add files via upload

---
+++ problem14.cpp
@@ -0,0 +1,32 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int main()
+{
+    int a,b;
+    string str;
+    while (getline(cin,str))
+	{
+        b = 1;
+        a = str.length();
+        for(int i = 0;i < a / 2; i++)
+	    {
+            if (str[i] != str[a - i - 1])
+	        {
+                b = 0;
+                break;
+            }       
+        }
+        if (b == 1)
+	    {
+            cout << "YES";       
+        }
+	    else
+	    {
+            cout << "NO";     
+        }
+        cout << endl;         
+    }
+    return 0;
+}
