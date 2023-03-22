6a9c6180c74fa49d8bbaeb1982d594b141d8bcd5
Weiwee0206 <4A8G0095@stust.edu.tw>
2023-03-23 00:08:14 +0800

    fix

---
+++ ITSA/ITSA_9.cpp
@@ -0,0 +1,17 @@
+﻿#include <iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int N, i, sum = 0;
+    cin >> N;
+    for (i = 1; i <= N; i++)
+    {
+        if (i % 3 == 0)     //找最近的3的倍數
+            sum += i;
+    }
+    cout << sum << endl;//得到總和
+
+    return 0;
+}
