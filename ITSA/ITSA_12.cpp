e857ef32ecb94d5b7c5bfa0babe63bf58e34c594
Weiwee0206 <4A8G0095@stust.edu.tw>
2023-03-22 20:09:42 +0800

    fix

---
+++ ITSA/ITSA_12.cpp
@@ -0,0 +1,23 @@
+﻿#include<iostream>
+#include<iomanip>
+using namespace std;
+
+
+int  f(int n) {
+    if (n == 0)
+        return 1;
+    else if (n == 1)
+        return 2;
+    else
+        return f(n - 1) + f(n / 2);
+}
+
+int main()
+{
+    int n;
+
+    while (cin >> n) {
+        cout << f(n) << endl;
+    }
+    return 0;
+}
