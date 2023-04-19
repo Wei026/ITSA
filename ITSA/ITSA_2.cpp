95660512aa230516d10d48d04104663522b3569d
How0904 <4a8g0118@stust.edu.tw>
2023-04-19 14:38:42 +0800

    EX_23

--- ITSA/ITSA_2.cpp
+++ ITSA/ITSA_2.cpp
@@ -1,14 +1,36 @@
-﻿#include<iostream>
-#include<iomanip>
+﻿#include <iostream> 
+#include <math.h> 
 using namespace std;
 
 int main() {
-
-	int input = 0;
-	cin >> input;
-	cout << fixed << setprecision(1) << (input * 1.6L * 10 / 10) << endl;
-	/*輸出<<指定浮點數表示<<小數點後幾位(預設為6)<<整數轉換小數(乘L)並確保小數點後僅一位<<換行*/
-
-
-	return 0;
+    int N, a1, a2, a3; //輸入錢,三種水果單價
+    int x;          //應找金額
+    int r50, r5, r1;  //找零50,5,1
+    scanf("%d,%d,%d,%d", &N, &a1, &a2, &a3);
+    if (a1 * 15 + a2 * 20 + a3 * 30 > N) {
+        cout << "0";     //如果錢少於金額 輸出0
+    }
+    else {
+        x = N - (a1 * 15 + a2 * 20 + a3 * 30); //x計算找零金額
+        if (x >= 50) {           //如果大於50從50開始找
+            r50 = floor(x / 50);
+            x = x - 50 * r50;
+            if (x >= 5) {           //5塊
+                r5 = floor(x / 5);
+                x = x - 5 * r5;
+            }
+            else {
+                r5 = 0;
+            }
+            if (x >= 1) {           //1塊
+                r1 = floor(x / 1);
+                x = x - 1 * r1;
+            }
+            else {
+                r1 = 0;
+            }
+            cout << r1 << "," << r5 << "," << r50; //輸出應找數量
+        }
+    }
+    return 0;
 }
